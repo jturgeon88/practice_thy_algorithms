2292a3ffa8cdca71112013173d72f8737b6a1505
Josh Turgeon <j_turgeon88@yahoo.com>
2018-07-26 16:26:14 -0500

    updated digital_root

--- Javascript/lib/digital_root.js
+++ Javascript/lib/digital_root.js
@@ -4,6 +4,20 @@
 // Keep repeating until there is only one digit in the result, called the "digital root".
 // Do not use string conversion within your method.
 function digitalRoot(number) {
+  // base case:
+  if (number < 10) {
+    return number;
+  }
+
+  // inductive step:
+  let sum = (number % 10) + digitalRoot((number - (number % 10))/10);
+
+  if (sum < 10) {
+    return sum;
+  } else {
+    return digitalRoot(sum);
+  }
+
 
 }
 module.exports = digitalRoot;
